a1cce8a96a2afaaf988b8761f1e9131f39122ae6
mashfaq6501 <mashfaq6501@altimetrik.com>
2022-06-24 17:01:27 +0530

    Added Project to repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import MainContent from './MainContent';
+import Navbar from './Navbar';
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
